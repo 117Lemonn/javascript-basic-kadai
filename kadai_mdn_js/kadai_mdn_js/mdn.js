f429ff11695b99c45aa572168523525f8395c7c4
117Lemonn <156330476+117Lemonn@users.noreply.github.com>
2024-06-12 08:30:18 +0900

    Delete kadai_mdn_js directory

--- kadai_mdn_js/kadai_mdn_js/mdn.js
+++
@@ -1,9 +0,0 @@
-const today = new Date();
-
-const year = today.getFullYear();  // 年
-const month = today.getMonth() + 1; // 月（
-const date = today.getDate();      // 日付
-
-const formattedDate = `${year}年${month}月${date}日`;
-
-console.log(formattedDate); 
